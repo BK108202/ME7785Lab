905265b4e0f3d8dc897660d3dfd9e1260da102a3
rhit-kangb3 <kangb3@rose-hulman.edu>
2025-03-11 18:56:40 -0400

    Change

--- Lab4_ws/src/teamfoobar_navigate_to_goal/launch/launch_navigate_to_goal.launch.py
+++ Lab4_ws/src/teamfoobar_navigate_to_goal/launch/launch_navigate_to_goal.launch.py
@@ -5,21 +5,21 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package='teamfoobar_navigate_to_goal',
-            executable='avoid_obstacle',
-            name='avoid_obstacle'
-        ),
-        Node(
-            package='teamfoobar_navigate_to_goal',
-            executable='get_object_range',
-            name='get_object_range'
-        ),
-        Node(
-            package='teamfoobar_navigate_to_goal',
-            executable='state_manager',
-            name='state_manager'
-        ),
+        # Node(
+        #     package='teamfoobar_navigate_to_goal',
+        #     executable='avoid_obstacle',
+        #     name='avoid_obstacle'
+        # ),
+        # Node(
+        #     package='teamfoobar_navigate_to_goal',
+        #     executable='get_object_range',
+        #     name='get_object_range'
+        # ),
+        # Node(
+        #     package='teamfoobar_navigate_to_goal',
+        #     executable='state_manager',
+        #     name='state_manager'
+        # ),
         Node(
             package='teamfoobar_navigate_to_goal',
             executable='go_to_goal',
